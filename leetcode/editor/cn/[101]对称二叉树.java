eac87455960bc19c884330ec075d06ffb53d3325
hklhai <hkhai@outlook.com>
2024-03-28 18:23:19 +0800

    add: #101

---
+++ leetcode/editor/cn/[101]对称二叉树.java
@@ -0,0 +1,65 @@
+//给你一个二叉树的根节点 root ， 检查它是否轴对称。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [1,2,2,3,4,4,3]
+//输出：true
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：root = [1,2,2,null,3,null,3]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [1, 1000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// 
+//
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？ 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 2684 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return dfs(root.left, root.right);
+    }
+
+    public boolean dfs(TreeNode l, TreeNode r) {
+        if (l == null && r == null) return true;
+        if (l == null || r == null) return false;
+        if(l.val != r.val) return false;
+        return dfs(l.left, r.right) && dfs(l.right, r.left);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
