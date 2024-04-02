c9e192ec54e93d3e87c1fb935d055116bd9f5a4e
hklhai <hkhai@outlook.com>
2024-04-02 16:39:56 +0800

    add: #34

---
+++ leetcode/editor/cn/[34]在排序数组中查找元素的第一个和最后一个位置.java
@@ -0,0 +1,75 @@
+//给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。 
+//
+// 如果数组中不存在目标值 target，返回 [-1, -1]。 
+//
+// 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [5,7,7,8,8,10], target = 8
+//输出：[3,4] 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [5,7,7,8,8,10], target = 6
+//输出：[-1,-1] 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [], target = 0
+//输出：[-1,-1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 10⁵ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// nums 是一个非递减数组 
+// -10⁹ <= target <= 10⁹ 
+// 
+//
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        if (nums.length == 0) return ans;
+        int n = nums.length;
+        if (target < nums[0] || target > nums[n - 1]) return ans;
+
+        // mid >= x
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        if (nums[r] != target) return ans;
+        ans[0] = r;
+
+        l = 0;
+        r = n - 1;
+        while (l < r) {
+            int mid = l + r + 1 >> 1;
+            if (nums[mid] <= target) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        ans[1] = l;
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
