8a22c85c598d0027cb53a779f27e4d3d8b29fae8
hklhai <hkhai@outlook.com>
2024-04-30 10:42:44 +0800

    fix: #560

--- leetcode/editor/cn/[560]和为 K 的子数组.java
+++ leetcode/editor/cn/[560]和为 K 的子数组.java
@@ -31,22 +31,25 @@
 // Related Topics 数组 哈希表 前缀和
 
 
+import java.util.HashMap;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int subarraySum(int[] nums, int k) {
         int n = nums.length;
-        // 前缀和数组，s[i] 表示从 nums[0] 到 nums[i-1] 的和
         int[] s = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             s[i] = s[i - 1] + nums[i - 1];
         }
-        // 哈希表用于存储某个前缀和出现的次数
-        Map<Integer, Integer> hash = new HashMap<>();
-        hash.put(0, 1); // 初始化，前缀和为0的情况有1种
+
+        Map<Integer, Integer> m = new HashMap<>();
+        m.put(0, 1);
+
         int res = 0;
         for (int i = 1; i <= n; i++) {
-            res += hash.getOrDefault(s[i] - k, 0); // 查找有多少个前缀和等于 s[i] - k
-            hash.put(s[i], hash.getOrDefault(s[i], 0) + 1); // 更新前缀和为 s[i] 的数量
+            int div = s[i] - k;
+            res += m.getOrDefault(div, 0);
+            m.put(s[i], m.getOrDefault(s[i], 0) + 1);
         }
         return res;
     }
